8cce6346e1c6e29be3cc95e71568b328efcfd4d6
Jenny Rigsjö <jenny.rigsjo@live.com>
2018-12-16 15:07:30 +0000

    Added links to report text for kmom05 and essay about website loading times in main navbar. Added style rules for images used in essay to stylsheet for kmom04.

--- config/navbar/responsive.php
+++ config/navbar/responsive.php
@@ -41,6 +41,11 @@ return [
                         "url" => "redovisning/kmom04",
                         "title" => "Redovisning för kmom04",
                     ],
+                    [
+                        "text" => "Kmom05",
+                        "url" => "redovisning/kmom05",
+                        "title" => "Redovisning för kmom05",
+                    ],
                 ],
             ],
         ],
@@ -93,6 +98,11 @@ return [
                         "text" => "Färg",
                         "url" => "rapport/fargschema",
                         "title" => "Rapport för kmom04",
+                    ],
+                    [
+                        "text" => "Laddningstid",
+                        "url" => "rapport/laddningstid",
+                        "title" => "Rapport för kmom05",
                     ],
                 ]
             ]
